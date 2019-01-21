670c27842d45cd6129ab190a2579212c3e946270
rr94 <rrajan@nseit.com>
2019-01-21 12:46:04 +0530

    test

--- date-util.py
+++
@@ -1,7 +0,0 @@
-# a module to define date utility functions
-
-# a function that returns date based on the
-# passed format
-
-# a function that returns only the month
-# for the passed current date
